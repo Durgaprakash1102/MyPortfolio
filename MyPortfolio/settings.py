ebd563c3ccdf7cab0e57ddd07c61a22812021621
Durga Prakash Shilarapu <109608138+Durgaprakash1102@users.noreply.github.com>
2025-05-19 21:02:37 +0530

    settings

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -66,10 +66,18 @@ WSGI_APPLICATION = 'MyPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DurgaPrakashPort$PortfolioDb',
+        'USER': 'DurgaPrakashPort',
+        'PASSWORD': 'Durgaprakash@1102',
+        'HOST': 'DurgaPrakashPortfolio.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 
 # Password validation
